2fa15d94bb036b5056a814e75de7a916dbd102c0
Omkar <omkargharat022@gmail.com>
2023-11-02 17:20:53 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Delievery_Dashboard.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
